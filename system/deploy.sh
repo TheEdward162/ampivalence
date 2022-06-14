03a48b0654cc0e3cd67459843549e9ab25f523e1
TheEdward162 <TheEdward162@gmail.com>
2022-06-14 21:34:37 +0200

    system: raw init

--- system/deploy.sh
+++ system/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-scp etc/local.d/fuckup-udev-database.start ampivalence:/etc/local.d/
-
 scp -r home/root/.profile home/root/bin ampivalence:/home/root/
 
 scp -r home/alfred/.profile home/alfred/.config ampivalence:/home/alfred/
